11e9feaf211f1846772c80128db66f8c6ab1ae10
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-12-24 20:38:13 -0300

    Avancei no tournament.py

--- Testing.py
+++ Testing.py
@@ -1,9 +1,8 @@
 from cs50 import get_string
-people: dict = {"name": "Alice", "city": "Seattle", "cake": "Chocolate"}
 
-s: str = "Abencads"
+a = [2,1]
+b = a
+b[0] = 3
 
-def test(x: str) -> str:
-    return "Hello"
 
-print(test(s))
+print(a)
