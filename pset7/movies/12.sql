9aa1ff6380108006ac7e6acdcb02a6eb7523d3bf
Gabriel Rabelo <163333836+Gr-moura@users.noreply.github.com>
2024-12-28 00:52:28 -0300

    Finished pset7/movies

---
+++ pset7/movies/12.sql
@@ -0,0 +1,4 @@
+SELECT title FROM movies WHERE movies.id IN 
+(SELECT stars.movie_id FROM stars WHERE stars.person_id = (SELECT id FROM people WHERE name = 'Johnny Depp'))
+AND movies.id IN
+(SELECT stars.movie_id FROM stars WHERE stars.person_id = (SELECT id FROM people WHERE name = 'Helena Bonham Carter'));
